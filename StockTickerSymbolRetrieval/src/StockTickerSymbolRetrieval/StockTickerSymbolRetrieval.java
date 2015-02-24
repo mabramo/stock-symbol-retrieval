189d2c576473ec11d5871532dfaecfd5b3e42723
Michael Abramo <abramomichael@gmail.com>
2015-02-24 12:06:16 -0500

    adding eclipse files

--- StockTickerSymbolRetrieval/src/StockTickerSymbolRetrieval/StockTickerSymbolRetrieval.java
+++ StockTickerSymbolRetrieval/src/StockTickerSymbolRetrieval/StockTickerSymbolRetrieval.java
@@ -1,10 +1,21 @@
 package StockTickerSymbolRetrieval;
 
-public class StockTickerSymbolRetrieval {
+public class StockTickerSymbolRetrieval 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] _args) 
+	{
 		// TODO Auto-generated method stub
 
+		//initialize objects and variables
+		//create 1 thread per stock exchange
+		//could be made fully dynamic by making a JSON of exchange + link to source of Stock Symbols
+		//begin symbol retrieval
 	}
 
+	private void getSymbols(String _exchangeName, String _URLString)
+	{
+		return;
+	}
+	
 }
